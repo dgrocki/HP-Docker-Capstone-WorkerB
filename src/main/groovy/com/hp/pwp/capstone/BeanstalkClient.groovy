de590338d7e636f618d1aa636d636eff3e7ff7a3
grockidile <grockidile@localhost.localdomain>
2019-02-03 11:04:09 -0800

    Worker_B can now pull data that is put on beanstalk by workmanager

--- src/main/groovy/com/hp/pwp/capstone/BeanstalkClient.groovy
+++ src/main/groovy/com/hp/pwp/capstone/BeanstalkClient.groovy
@@ -20,21 +20,36 @@ class BeanstalkClient{
 		byte[] data = json.getBytes();
 
 		connection.put(priority, delaySeconds, timeToRun, data);
-
 	}	
-	public JobImpl recieveWork(){
-		currentJob = connection.reserve();
-		return currentJob;
-	}
-	public void deleteWork(long id){	//when should we delete the job off of the queue?
-		connection.delete(id);
-
-	}
+	//functionhere for testing purposes
 	public void useTube(String s){
 		connection.useTube(s);
 	}
-	public void watchTube(String s){
-		connection.watch(s);
+	//pull a new job off the the to_worker_b queue
+	public String recieve_new_work(){
+		connection.watch("to_worker_b");
+			
+		JobImpl job = connection.reserve();
+		String s = new String(job.data);
+		println s;
+		
+		connection.delete(job.jobId);
+			
+		return s;
 	}
+	//put a new job on the riak queue
+	public void send_to_work_manager(String json){
+		connection.useTube("riak");
+		sendWork(json);
+		
+		
+	}
+	//put a job on the status queue
+	public void send_status(String json){
+		connection.usetTube("status");
+		sendWork(json);
+	}
+
+
 
 }
--- src/main/groovy/com/hp/pwp/capstone/WorkerB.groovy
+++ src/main/groovy/com/hp/pwp/capstone/WorkerB.groovy
@@ -4,63 +4,12 @@ import com.surftools.BeanstalkClientImpl.JobImpl
 import com.surftools.BeanstalkClient.BeanstalkException
 
 class WorkerB{
-	private BeanstalkClient beanstalk = new BeanstalkClient();		
-	//pull a new job off the new_work queue
-	public recieve_new_work(){
-		beanstalk.watchTube("new_work");
-			
-		JobImpl job = beanstalk.recieveWork();
-		
-		String s = new String(job.data);
-		println s;
-		
-		beanstalk.deleteWork(job.jobId);
-			
-		return;
-	}
-	//put a new job on the to_workerB queue
-	public send_to_workerB(){}
-	//pull a job off of the riak queue
-	public send_to_riak(){
-		beanstalk.watchTube("riak");
-		JobImpl job = beanstalk.recieveWork();
-		
-		String s = new String(job.data);
-		println s;
-		
-		beanstalk.deleteWork(job.jobId);
-			
-		return;
-
-	}
-	//put a job on the status queue
-	public send_status(){}
-
 
 	public static void main(String [] args) {
-		WorkerB wm = new WorkerB();
-
-		//for testing only, only need a beanstalk client in the class
 		BeanstalkClient beanstalk = new BeanstalkClient();		
-	
-		beanstalk.useTube("new_work");
-		String input = "New work from the outside world";	
-		beanstalk.sendWork(input);
-		beanstalk.useTube("riak");
-		input = "More stuff for Riak";	
-		beanstalk.sendWork(input);
-		beanstalk.useTube("new_work");
-		input = "New work number 2 from the outside world";	
-		beanstalk.sendWork(input);
-		beanstalk.useTube("riak");
-		input = "Time to put something in Riak from Worker B";	
-		beanstalk.sendWork(input);
-		
-		
-		wm.recieve_new_work();
-		wm.send_to_riak();
-		wm.recieve_new_work();
-		wm.send_to_riak();
+		while(1){
+			beanstalk.recieve_new_work();
+		}
 		return;
 	}
 
