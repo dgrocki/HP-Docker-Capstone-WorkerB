2bfc4a71d4bac8e3fc631dc01120070ef64bff91
grockidile <grockidile@10-248-194-219.wireless.oregonstate.edu>
2019-03-12 20:57:23 -0700

    working with pdf

--- src/main/groovy/com/hp/pwp/capstone/WorkerB.groovy
+++ src/main/groovy/com/hp/pwp/capstone/WorkerB.groovy
@@ -6,13 +6,17 @@ import com.surftools.BeanstalkClient.BeanstalkException
 class WorkerB{
 
 	public static void main(String [] args) {
-		BeanstalkClient beanstalk = new BeanstalkClient();		
+		BeanstalkClient beanstalk = new BeanstalkClient();  
 		while(1){
+
 			String s = beanstalk.recieve_new_work();
+		
 			println "Recieved from workerManager: " + s;
-			println "Doing Work in Worker B";
-			sleep (2000);
-			beanstalk.send_to_work_manager(s + " +  Worker B is done");
+						
+			PDFFormat imposition = new PDFFormat(s);
+			imposition.start();	
+
+			beanstalk.send_to_work_manager(s);
 			
 		}
 		return;
